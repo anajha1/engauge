410ae607daa19bf96ea3bc89e542db7c30cf4fb1
Sam Ping <samuelping42@gmail.com>
2021-01-15 23:41:12 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,84 +1,59 @@
 var canvas = document.getElementById("snapshot-1");
 var context = canvas.getContext("2d");
 
-// var canvas2 = document.getElementById("snapshot-2")
-// var context2 = canvas2.getContext("2d")
-
 const video = document.querySelector("#myVidPlayer");
 
 var w, h;
 canvas.style.display = "none";
-// canvas2.style.display = "none"
 
-// function dataURLtoFile(dataurl, filename) {
-//     var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-//         bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-//     while(n--){
-//         u8arr[n] = bstr.charCodeAt(n);
-//     }
-//     return new File([u8arr], filename, {type:mime});
-// }
-// function b64toBlob(dataURI) {
+// converts base64 string to blob
+function b64toBlob(dataURI) {
+  var byteString = atob(dataURI.split(",")[1]);
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
 
-//   var byteString = atob(dataURI.split(',')[1]);
-//   var ab = new ArrayBuffer(byteString.length);
-//   var ia = new Uint8Array(ab);
-
-//   for (var i = 0; i < byteString.length; i++) {
-//     ia[i] = byteString.charCodeAt(i);
-//   }
-//   return new Blob([ab], { type: 'image/jpeg' });
-// }
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: "image/jpeg" });
+}
 
 function snapshot() {
   context.fillRect(0, 0, w, h);
   context.drawImage(video, 0, 0, w, h);
   canvas.style.display = "block";
 
-  var dataURL = canvas.toDataURL("image/jpeg", 0.5); // converts snapshot to base64
-  console.log(dataURL);
-  // var imageFile = b64toBlob(dataURL)
+  var dataURL = canvas.toDataURL("image/jpeg", 0.5); // converts snapshot to base64, at medium quality
+  // console.log(dataURL);
 
-  // var imageFile = atob(dataURL);
-  // var imageFile = dataURLtoFile(dataURL, "photo.jpeg");
+  var blob = dataURLtoFile(dataURL, "photo.jpeg"); // converts image (in base64 form) to blob
 
-  // var imageFile = Buffer.from(dataURL, 'base64')
+  var raw = JSON.stringify({ Url: blob }); // convert it to request-able body
 
-  // canvas.toBlob(function (blob) {
   // preparing to make post request to custom vision model
-  fetch(dataURL)
-    .then((res) => res.blob())
-    .then((blob) => {
-      console.log(blob);
-      var url = window.URL.createObjectURL(blob);
-      var myHeaders = new Headers();
-      myHeaders.append("Prediction-Key", "7a5b384c2a3041f2a998bf8d0f68a85a");
-      myHeaders.append("Content-Body", "application/json");
-      myHeaders.append("Content-Type", "application/octet-stream");
+  var myHeaders = new Headers();
+  myHeaders.append("Prediction-Key", "7a5b384c2a3041f2a998bf8d0f68a85a");
+  myHeaders.append("Content-Body", "application/json");
+  myHeaders.append("Content-Type", "application/octet-stream");
 
-      var raw = JSON.stringify({ Url: blob });
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-      var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
-
-      // make post request to custom vision model
-      fetch(
-        "https://engauge.cognitiveservices.azure.com/customvision/v3.0/Prediction/b2f0ff86-4d04-43b3-a17b-91223e124ea6/classify/iterations/Iteration2/url",
-        requestOptions
-      )
-        .then((response) => response.text())
-        .then((result) => {
-          // display results on webpage and console
-          console.log(result);
-          document.getElementById("returned-data").innerHTML = result;
-        })
-        .catch((error) => console.log("error", error));
-    });
-  // }, 'image/jpeg', 0.50)
+  // make post request to custom vision model
+  fetch(
+    "https://engauge.cognitiveservices.azure.com/customvision/v3.0/Prediction/b2f0ff86-4d04-43b3-a17b-91223e124ea6/classify/iterations/Iteration5/url",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => {
+      // display results on webpage
+      document.getElementById("returned-data").innerHTML = result;
+    })
+    .catch((error) => console.log("error", error));
 }
 
 window.navigator.mediaDevices
